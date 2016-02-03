49409fa35de9d7d3cc702d03bd1776d6f7dff4ec
Kristian Hellang <kristian@hellang.com>
2016-02-03 16:56:40 +0100

    Bump version

--- LangVersionFixer/Properties/AssemblyInfo.cs
+++ LangVersionFixer/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("ba839f37-bcd2-418f-967e-aa49cc905ed6")]
 
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersion("1.3.0")]
